c8ff77c5a6e3ea9a9fb771b8b7ee5f1cc8ba9326
Rei Tan <tanreiyi@gmail.com>
2024-08-09 02:44:33 -0500

    remove error log

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -8,7 +8,6 @@ const createUser = (req, res) => {
         return res.status(201).json({message: "Successfully created user"});
     }
     catch (error) {
-        console.error(error); 
         return res.status(500).json({error: "Error creating user in database"});
     }
 }
